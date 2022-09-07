fea56c8cd88de3a42250c00046510e4ffafeec56
Roman Garza <roman.garza@cascadefintech.com>
2022-09-06 17:43:36 -0700

    recent activity & load cash functions added

--- cypress/e2e/MyAccount/dashboard_page.cy.js
+++ cypress/e2e/MyAccount/dashboard_page.cy.js
@@ -5,22 +5,23 @@ as buttons to lead the user to various pages and quick
 information like account balance and recent activity. */
 
 describe("Test Dashboard main functions", () => {
-  beforeEach(() => {
+  it("logs in", () => {
     cy.visit("https://cvtmyaccount.thatprepaidcard.com/");
     //   Login custom command
     cy.Login();
-  });
-
-  it("See Card Number", () => {
-    cy.get('[data-cy="dash-virtualcard-img-btn"] > .v-btn__content')
-      .should("be.visible")
-      .click();
-      cy.location("pathname").should("eq", "/dash/virtualcard")
   })
+
+  // Code required to view virtual card
+  // it("See Card Number", () => {
+  //   cy.get('[data-cy="dash-virtualcard-img-btn"] > .v-btn__content')
+  //     .should("be.visible")
+  //     .click();
+  //     cy.location("pathname").should("eq", "/dash/virtualcard")
+  // })
 
   it("Account Deposit", () => {
     cy.get('[data-cy="amount-text"]')
-    .should("be.visible")
+      .should("be.visible")
   })
 
   it("Direct Deposit", () => {
@@ -30,6 +31,27 @@ describe("Test Dashboard main functions", () => {
       .click();
     cy.location("pathname").should("eq", "/dash/directDeposit");
   });
+
+  it("Recent Activity", () => {
+    cy.visit("https://cvtmyaccount.thatprepaidcard.com/");
+    cy.Login();
+    cy.get('[data-cy="dash-recentActivity-title"]')
+      .should("be.visible")
+    cy.get('.transactions')
+      .should("be.visible")
+    cy.get('[data-cy="dash-recentActivity-button"]')
+      .should("be.visible")
+      .click()
+    cy.location("pathname").should("eq", "/dash/activity")
+  });
+
+  it("Load Cash", () => {
+    cy.visit("https://cvtmyaccount.thatprepaidcard.com/");
+    cy.Login();
+    cy.get('[data-cy="dash-loadMoney-title"]').should("be.visible")
+    cy.get('[data-cy="dash-loadMoney-button"]').should("be.visible").click()
+    cy.get('.v-card__actions').should("be.visible")
+  })
 });
 
 // Expected results
